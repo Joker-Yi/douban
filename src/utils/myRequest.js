e12afe69c5d4dd959e541beb32553b78a7b89754
Joker-Yi <735734221@qq.com>
2019-08-12 10:14:51 +0800

    封装wx请求

---
+++ src/utils/myRequest.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Yee .
+ */
+
+function myRequest(options){
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url: options.url,//请求的路径
+            method: options.method || 'get',//请求方式
+            data: options.data || {},//请求参数
+            header: options.header || {},//请求头
+            scuccess: function (res) {
+                //成功后的回调
+                resolve(res)
+            },
+            fail(err){
+                //失败后的回调
+                reject(err)
+            }
+        })
+    })
+}
+//方法暴露出去                                                                             
+export default myRequest;
+
+
+
+
+
+
+
+
+
